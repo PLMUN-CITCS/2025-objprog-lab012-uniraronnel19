9d23e8884f5fde0de0bc2c41edfaaddadd986fee
uniraronnel19 <uniraronnel_bsit@plmun.edu.ph>
2025-02-19 14:28:12 -0700

    Submitting OBJPROG Week 05 - Laboratory # 12

---
+++ MethodDemo.java
@@ -0,0 +1,15 @@
+public class MethodDemo {
+    public static void main(String[] args) {
+        greet(); // Calling the greet method
+        printMessage("This is my message!");
+    }
+    
+    public static void greet() {
+        System.out.println("Hello from the greet method!");
+    }
+    
+    public static void printMessage(String message) {
+        System.out.println(message);
+    
+    }
+}
